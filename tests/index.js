90124705e34928b79fc9d3b65a2a0cdfdbab3f07
nojaf <florian.verdonck@outlook.com>
2024-11-09 22:29:54 +0100

    Initial test setup

---
+++ tests/index.js
@@ -0,0 +1,31 @@
+import { execSync } from "child_process";
+import { fileURLToPath } from "url";
+import * as path from "path";
+import { exit } from "process";
+
+const currentFileName = fileURLToPath(import.meta.url);
+const currentDir = path.dirname(currentFileName);
+const repoRoot = path.resolve(currentDir, "..");
+const testsDir = path.join(repoRoot, "tests");
+
+// Compile all tests
+execSync("npx rewatch", { cwd: repoRoot, stdio: "inherit" });
+
+const successGreen = "\x1b[32m";
+const warningYellow = "\x1b[33m";
+const resetColor = "\x1b[0m";
+
+// Assert nothing changed
+const gitDff = execSync("git ls-files --modified .", {
+  cwd: testsDir,
+}).toString();
+if (!gitDff) {
+  console.log(`${successGreen}✅ No unstaged tests difference!${resetColor}`);
+  exit(0);
+} else {
+  console.log(
+    `${warningYellow}⚠️ There are unstaged differences in tests! Did you break a test?\n${gitDff}${resetColor}`,
+  );
+  execSync("git --no-pager diff .", { stdio: "inherit" });
+  exit(1);
+}
