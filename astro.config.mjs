594778875b1408464ec44360b748a9d552f75621
nojaf <florian.verdonck@outlook.com>
2024-11-10 15:58:43 +0100

    Initial starlight website

---
+++ astro.config.mjs
@@ -0,0 +1,26 @@
+import { readFileSync } from "node:fs";
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+const rescriptTM = JSON.parse(
+  readFileSync("./docs/assets/rescript.tmLanguage.json", "utf-8"),
+);
+
+export default defineConfig({
+  srcDir: "docs",
+  publicDir: "docs/public",
+  integrations: [
+    starlight({
+      title: "Experimental WebAPI",
+      logo: {
+        src: "./docs/assets/rescript-logo.svg",
+      },
+      customCss: ["./docs/styles/fonts.css", "./docs/styles/theme.css"],
+      expressiveCode: {
+        shiki: {
+          langs: [rescriptTM],
+        },
+      },
+    }),
+  ],
+});
